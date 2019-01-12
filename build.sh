3d5bcfda2aae8d495d32720a10564eb9578977b2
HiedaNaKan <HiedaNaKan@kurumi.io>
2019-01-13 06:54:13 +0800

    update build.sh

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@ for jar in ./libs/*.jar;do
  export CLASSPATH=$CLASSPATH:$jar
 done
 
-javac -classpath $CLASSPATH -sourcepath ./src
+javac -classpath $CLASSPATH -sourcepath $(dirname $(readlink -f $0))/src
