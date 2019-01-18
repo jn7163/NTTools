63bc64a0ebaa912bfa4e14fa5839fb0c7d5840d9
HiedaNaKan <HiedaNaKan@kurumi.io>
2019-01-18 18:48:27 +0800

    test

--- src/io/kurumi/nt/bots/StatusListenerBotAdapter.java
+++ src/io/kurumi/nt/bots/StatusListenerBotAdapter.java
@@ -3,47 +3,64 @@ package io.kurumi.nt.bots;
 import io.kurumi.nt.twitter.*;
 import twitter4j.*;
 import io.kurumi.nt.*;
+import java.util.concurrent.*;
 
 public class StatusListenerBotAdapter extends StatusAdapter {
 
     private TwiAccount acc;
     private StatusListenerBot bot;
     private Twitter api;
-    
+
     private long statusId;
+
+    private ExecutorService pool = Executors.newScheduledThreadPool(9);
 
     public StatusListenerBotAdapter(TwiAccount acc, StatusListenerBot bot) {
         this.acc = acc;
         this.bot = bot;
         api = acc.createApi();
         statusId = bot.getStatusId(acc);
-        
-        
+
+
         System.out.println("「" + bot.getBotName() + "」"  + "正在监听 : " + statusId);
     }
-    
-    
-    @Override
-    public void onStatus(Status status) {
-        
-        if (status.getInReplyToStatusId() == statusId) {
-            
-            System.out.println("「" + bot.getBotName() + "」" + "正在处理..");
-            
-            try {
-                
-                bot.onStatus(api, status);
-                
-            } catch (TwitterException e) {
-                
-                System.out.println("「" + bot.getBotName() + "」"  + "出错 : " + e);
-                
-            }
 
-        }
-        
+
+    @Override
+    public void onStatus(final Status status) {
+
+        pool.execute(new Runnable() {
+
+                @Override
+                public void run() {
+
+                    if (status.getInReplyToStatusId() == statusId) {
+
+                        System.out.println("「" + bot.getBotName() + "」" + "正在处理..");
+
+                        try {
+
+                            bot.onStatus(api, status);
+
+                        } catch (TwitterException e) {
+
+                            System.out.println("「" + bot.getBotName() + "」"  + "出错 : " + e);
+
+                        }
+
+
+
+                    }
+
+
+                }
+
+            });
+
     }
-    
-    
-    
+
+
+
+
+
 }
