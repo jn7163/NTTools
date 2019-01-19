a98b3e08c786454d50c989a773223b368e6ad511
HiedaNaKan <HiedaNaKan@kurumi.io>
2019-01-19 09:50:21 +0800

    判断文本情感分析是否超过大小

--- src/io/kurumi/nt/taip/AIUtil.java
+++ src/io/kurumi/nt/taip/AIUtil.java
@@ -2,6 +2,7 @@ package io.kurumi.nt.taip;
 
 import cn.xsshome.taip.nlp.*;
 import cn.hutool.json.*;
+import cn.hutool.core.util.*;
 
 public class AIUtil {
 
@@ -11,9 +12,18 @@ public class AIUtil {
     public static TAipNlp nlp = new TAipNlp(id, key);
 
     public static float nlpTextpolar(String text) throws Exception {
-
-
-        JSONObject obj = new JSONObject(nlp.nlpTextpolar(text)).getJSONObject("data");
+        
+        byte[] tb = text.getBytes(CharsetUtil.CHARSET_UTF_8);
+        
+        if (tb.length > 200){
+            
+            byte[] nb =  new byte[200];
+            System.arraycopy(tb,0,nb,0,200);
+            tb = nb;
+            
+        }
+        
+        JSONObject obj = new JSONObject(nlp.nlpTextpolar(StrUtil.str(tb,"utf-8"))).getJSONObject("data");
 
         int polar = obj.getInt("polar");
         float confd = obj.getFloat("confd");
